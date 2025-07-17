bbb5cdb93d43063f505532c47899db9a0ed85ccb
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-07-17 12:37:53 +1000

    📁pushed up folder structure

---
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+//JUST FILLER SO THAT DIRECTORIES ARE PUSHED UP
+import classes from "./LandingPage.module.css"
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className={classes.wrapper}>
+      Components Here!
+    </div>
+  )
+}
+
+export default LandingPage
