af39429e728c7499474f02113b75dd7693d4f35e
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-08-29 12:48:00 +1000

    set up express + session storage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,93 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import session, { Session } from "express-session";
+import cors from "cors";
+import { RedisStore } from "connect-redis";
+import { createClient } from "redis";
+import { config } from "dotenv";
 
+declare module "express-session" {
+  interface SessionData {
+    userId: number;
+  }
+}
+
+// Initialize client.
+if (
+  process.env["REDIS_PORT"] === undefined ||
+  process.env["REDIS_PORT"] === ""
+) {
+  console.log(process.env);
+  console.error("Redis port not provided in .env file");
+  process.exit(1);
+}
+
+let allowed_origins;
+if (
+  process.env["ALLOWED_ORIGINS"] === undefined ||
+  process.env["ALLOWED_ORIGINS"] === ""
+) {
+  console.log("Warning: ALLOWED_ORIGINS not specified. Using wildcard *.");
+  allowed_origins = ["*"];
+} else {
+  allowed_origins = process.env["ALLOWED_ORIGINS"]?.split(",");
+}
+
+let redisClient = createClient({
+  url: `redis://localhost:${process.env["REDIS_PORT"]}`,
+});
+
+redisClient.connect().catch(console.error);
+
+// Initialize store.
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "session:",
+});
+
+const app = express();
+const SERVER_PORT = 5180;
+const SALT_ROUNDS = 10;
+
+app.use(
+  cors({
+    origin: allowed_origins,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "20mb" }));
+
+if (process.env["SESSION_SECRET"] === undefined) {
+  console.error("Session secret not provided in .env file");
+  process.exit(1);
+}
+
+if (
+  process.env["DATABASE_URL"] === undefined ||
+  process.env["DIRECT_URL"] === undefined
+) {
+  console.error("Database URL or Direct URL not provided in .env file.");
+  process.exit(1);
+}
+
+// Initialize session storage.
+app.use(
+  session({
+    store: redisStore,
+    resave: false, // required: force lightweight session keep alive (touch)
+    saveUninitialized: false, // recommended: only save session when data exists
+    secret: process.env["SESSION_SECRET"],
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("Hello, TypeScript with Express :)))!");
+  res.send("Hello, TypeScript with Express :)))!");
+});
+
+if (process.env["NODE_ENV"] !== "test") {
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server running on port http://localhost:${SERVER_PORT}`);
+  });
+}
+
+export default app;
