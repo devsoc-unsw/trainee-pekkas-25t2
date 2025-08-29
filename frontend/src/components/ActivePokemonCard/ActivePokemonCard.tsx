c796fd53b8fc26c6e6cfdf038fa46d3b5d27f64c
Andrei Olivar <andrei.olivar78@gmail.com>
2025-08-29 14:10:39 +1000

    feat: add styles for pokemon avatar in active pokemon card

--- frontend/src/components/ActivePokemonCard/ActivePokemonCard.tsx
+++ frontend/src/components/ActivePokemonCard/ActivePokemonCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../Card/Card'
 import styles from './ActivePokemonCard.module.css'
 import SwapIcon from '../../assets/swap-icon.svg'
+import PokemonExampleIcon from '../../assets/pokemon-example-icon.png'
 
 function ActivePokemonCard() {
   return (
@@ -10,6 +11,12 @@ function ActivePokemonCard() {
         <button type="button" className={styles.swapButton}>
           <img src={SwapIcon} alt="swap icon" className={styles.swapButtonIcon} />
         </button>
+      </div>
+      <div className={styles.pokemonContainer}>
+        <div className={styles.pokemonProfileContainer}>
+          <img src={PokemonExampleIcon} alt="image of a pokemon" className={styles.pokemonIcon} />
+        </div>
+        <div className={styles.pokemonLevelContainer}>10</div>
       </div>
     </Card>
   )
