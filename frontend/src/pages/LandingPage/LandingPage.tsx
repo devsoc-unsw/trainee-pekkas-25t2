9fd87f8268e8e8a09be49a0017e5cd5aa034448e
Eugene Briones <eugenebriones7135@gmail.com>
2025-09-01 16:37:56 +1000

    landing page fix

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,13 @@
 import classes from "./LandingPage.module.css"
 
+import NavBar from "../../components/Navbar/Navbar"
+
 function LandingPage() {
-    return (
-        <div className={classes.wrapper}>
-            <Header />
-            <NavBar />
-            <ActivePokemonCard/>
-        </div>
-    )
+  return (
+    <div className={classes.wrapper}>
+      <NavBar />
+    </div>
+  )
 }
 
 export default LandingPage
