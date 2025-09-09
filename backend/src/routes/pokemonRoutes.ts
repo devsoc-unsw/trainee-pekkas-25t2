2cce593a47ed685025069dc8139a397979110d7c
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-09-09 19:42:41 +1000

    🧹evo logic change to pokemon tables

--- backend/src/routes/pokemonRoutes.ts
+++ backend/src/routes/pokemonRoutes.ts
@@ -5,5 +5,5 @@ const router = express.Router();
 
 //this shouldn't be called highk mainly just so we can test the endpoint
 router.get("/random", pokemonController.getRandomPokemon);
-
+router.get("/owned");
 export default router;
