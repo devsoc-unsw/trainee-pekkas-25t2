d2656f05eb297285150c89c765284df86b575ee5
Andrei Olivar <andrei.olivar78@gmail.com>
2025-09-10 13:42:04 +1000

    feat: add loading option for primary button

--- frontend/src/components/PrimaryButton/PrimaryButton.tsx
+++ frontend/src/components/PrimaryButton/PrimaryButton.tsx
@@ -6,9 +6,13 @@ type PrimaryButtonProps = {
   onClick?: () => void;
   disabled?: boolean;
   children?: React.ReactNode;
+  loading?: boolean
 }
 
-function PrimaryButton({ type, onClick, disabled, children }: PrimaryButtonProps) {
+function PrimaryButton({ type, onClick, disabled, loading, children }: PrimaryButtonProps) {
+  if (loading)
+    return <div className={styles.loading}/>
+  
   return (
     <button 
       type={type ?? "button"}
